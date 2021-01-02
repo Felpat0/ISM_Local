1a838c2a521f7042db26ee1fbbae58c07ad8f145
hrmntmrs <50423661+hrmntmrs@users.noreply.github.com>
2021-01-02 12:33:35 +0100

    aggiunta sezione per profilo utente offerente
    vanno sistemati i controlli che attualmente non possono essere effettuati (mancano le pagine da cui ci si arriva)
    va inserita la questione delle recensioni

--- js/profiloUtente.js
+++ js/profiloUtente.js
@@ -1,10 +1,10 @@
 
-  //if(localStorage["tipoUtente" == "anziano"]){
+  //if(localStorage["tipoUtente" == "offerente"]){ //il controllo che va fatto è se l'utente è offerente
     const url= ip + '/profiloUtente/profiloAnziano.php';
     var http = new XMLHttpRequest();
     http.open("POST", url, true);
     http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    var vars = "id=1"; //+ localStorage["id"];
+    var vars = "id=1"; //+ localStorage["id"]; bisogna passare l'id dell'anziano in altri modi
 
     http.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -21,6 +21,27 @@
 
       http.send(vars);
       
- // } else if(localStorage["tipoUtente" == "offerente"]){}
+ /* } else if(localStorage["tipoUtente" == "anziano"]){
+      const url= ip + '/profiloUtente/profiloOfferente.php';
+      var http = new XMLHttpRequest();
+      http.open("POST", url, true);
+      http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      var vars = "id=1"; //+ localStorage["id"]; bisogna passare l'id dell'offerente in altri modi
+
+      http.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+              console.log(http.responseText);
+              var result = JSON.parse(http.responseText);
+
+              document.getElementById('nome').innerHTML = result['0']['nome'];
+              document.getElementById('cognome').innerHTML = result['0']['cognome'];
+              //document.getElementById('nascita').innerHTML = result['0']['nome'];
+              document.getElementById('telefono').innerHTML = result['0']['telefono'];
+              document.getElementById('email').innerHTML = result['0']['email'];
+          }
+        };
+
+        http.send(vars);
+ }*/
 
   

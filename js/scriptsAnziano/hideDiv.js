23f239c91e37d370458264504b97c3532487e346
hrmntmrs <50423661+hrmntmrs@users.noreply.github.com>
2021-01-03 12:51:57 +0100

    Script per nascondere i div

---
+++ js/scriptsAnziano/hideDiv.js
@@ -0,0 +1,7 @@
+
+function hideDiv(div1, div2){ //nasconde div1 e mostra div2
+    var x = document.getElementById(div1);
+    var y = document.getElementById(div2);
+    x.style.display = "none";
+    y.style.display = "block";
+}

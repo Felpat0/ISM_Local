cd7dc22debec8949675267663e9e26c6a3f1225a
Felpat0 <b994@live.com>
2021-02-27 10:39:18 +0100

    Fix back button

--- js/ip.js
+++ js/ip.js
@@ -1,1 +1,10 @@
 var ip = "http://www.gograndpa.altervista.org";
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+  document.addEventListener("backbutton", onBackKeyDown, false);
+}
+
+function onBackKeyDown() {
+  window.location.href = "login.html";
+}
